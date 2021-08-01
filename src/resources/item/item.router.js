30254d26f5e2cd63d218d5e44bb42956490b2159
Robert Crocker <robert@vizsimply.com>
2021-08-01 14:50:34 -0400

    Complete lesson 2.

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
+
+// Mounted at api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// mounted at /api/item/:id
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
